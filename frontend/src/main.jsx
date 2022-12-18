780d3f07f93a45b005d0bd4963f74b168f8655ec
muwaahib <muhaabdi71@gmail.com>
2022-12-18 06:27:36 +0300

    DONE

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import reportWebVitals from './'
-import {GoogleOAuthProvider} from '@react-oauth/google'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider >  <App /></GoogleOAuthProvider>;
+     <Router>
+     <Provider store={store}>
+    <App />
+    </Provider>
+     </Router>
+   
+   
    
   </React.StrictMode>
 )
