0d7de018c158472db29f4f2778b6e909191ede15
sakariye10 <sakariyesaeed51@gmail.com>
2022-12-19 18:16:21 +0300

    done

---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+const User = () => {
+    return (
+      <div>
+        <div>
+          <h1>hello world</h1>
+        </div>
+        <img src="saki.png" alt="" srcset="" />
+      </div>
+    );
+}
+ 
+export default User;
