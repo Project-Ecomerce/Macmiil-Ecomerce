bc1a5f7496d4dbc691666b53f46f92a649b816ec
Saed Oday Somaliyeed <Smuuse09@gmail.com>
2022-12-24 17:40:06 +0300

    Updated

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,11 +6,11 @@ import {HiUsers} from "react-icons/hi"
 import {MdOutlineProductionQuantityLimits} from "react-icons/md"
 function Sidebar() {
   return (
-    <div className='Container '>
-        <div className="Sidebar flex bg-green-300 min-h-screen w-72 p-5">
+    <section className=' flex gap-6border'>
+        <div className="Sidebar flex  min-h-screen w-78 p-5">
           <div className="top_section">
-            <div className="bars flex  gap-28 items-center ">
-            <h1 className="logo text-green-600 ">Macmiil Online</h1>
+            <div className="bars flex  gap-24 items-center  ">
+            <h1 className="logo text-green-600 ">Macmiil Online  </h1>
            <img className='w-5' src="icons8_menu_500px_2.png" alt="" />
             </div>
             <menu className='mt-12'>
@@ -19,52 +19,58 @@ function Sidebar() {
                 <h1>Dashboard</h1>
                </div></Link>
 
-               <div className="div flex items-center gap-5  p-3 cursor-pointer hover:bg-green-400 ">
+               <div className="div flex items-center gap-5  p-3 cursor-pointer hover:bg-green-400 rounded-full ">
                <MdOutlineProductionQuantityLimits/>
                 <h1>Products</h1>
                </div>
 
-               <div className="div flex items-center gap-5 p-3 hover:bg-green-400  ">
+               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 rounded-full  ">
                <MdOutlineLibraryAdd/>
                 <h1>Add Products</h1>
                </div>
 
 
-               <div className="div flex items-center gap-5 p-3 hover:bg-green-400  transition-all  ">
+               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 cursor-pointer  transition-all rounded-full  ">
                <HiUsers/>
                 <h1>Users</h1>
                </div>
               
-               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all  ">
+               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all cursor-pointer rounded-full  ">
                <HiUsers/>
                 <h1>Sub Catogary</h1>
                </div>
 
-               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all   ">
+               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all cursor-pointer rounded-full  ">
                <HiUsers/>
                 <h1>Sub Catogary</h1>
                </div>
 
-               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all   ">
+               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all cursor-pointer  rounded-full ">
                <MdOutlineLibraryAdd/>
                 <h1>Add Products</h1>
                </div>
 
-               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all  ">
+               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all cursor-pointer  rounded-full ">
                <MdOutlineLibraryAdd/>
                 <h1>Add Products</h1>
                </div>
 
-               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all  ">
+               <div className="div flex items-center gap-5 p-3 hover:bg-green-400 transition-all cursor-pointer shadow-sm ">
                <FaHome/>
                 <h1>Dashboard</h1>
                </div>
 
             </menu>
             </div>  
+           
         </div>
- 
- </div>
+        <div className=" w-full h-16 text-xl text-gray-900 font-semibold bg-[#8CFFCA] p-5 border">
+       <div className=''>
+        <h1>Sidebar hello</h1>
+       </div>
+      </div>
+        </section>
+
   )
 }
 
